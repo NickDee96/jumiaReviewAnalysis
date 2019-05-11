2a7724393828abf9da25be75c819bbdbc4c29f38
NickDee96 <mumeronick@gmail.com>
2019-05-11 06:38:50 +0300

    second commit

--- scraper.py
+++ scraper.py
@@ -1,26 +1,63 @@
 import json
 import requests as req
-from bs4 import BeautifulSoup as soup
+import pandas as pd
+import random
+
+
+def getHeader():
+    headers=(
+            {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'},
+            {'user-agent':'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'},
+            {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'},
+            {'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'},
+            {'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'},
+            {'user-agent':'Mozilla/5.0 (X11; U; Linux Core i7-4980HQ; de; rv:32.0; compatible; JobboerseBot; http://www.jobboerse.com/bot.htm) Gecko/20100101 Firefox/38.0'},
+            {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0'},
+            {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0'},
+            {'user-agent':'Mozilla/5.0 (X11; od-database-crawler) Gecko/20100101 Firefox/52.0'},
+            {'user-agent':'Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.2.16) Gecko/20110323 Ubuntu/10.04 (lucid) Firefox/3.6.16'},
+        )
+    return random.choice(headers)
+
+
+
 
 def maxRevFinder(url):
-    page=req.get(url,'lxml')
+    page=soup(req.get(url,headers=getHeader() ).text,'lxml')
     containers=page.findAll('div',{'class':'-gallery'})
+    a=pd.DataFrame(columns=('Name','Brand','SKU','Link','Rating','Reviews'))
     for i in containers:
-        id=i['data-sku']
-        brand=i.findAll('span',{'class':'brand'})[0].text.strip()
-        width=i.findAll('div',{'class':'stars'})[0]['style']
-        rating=width.replace('%','').split(': ')[1]
-        revs=i.findAll('div',{'class':'total-ratings'})[0].text
-        for r in [('(',''),(')','')]:
-            revs=revs.replace(*r)
+        try:
+            id=i['data-sku']
+            pLink=i.a['href']
+            pName=i.findAll('span',{'class':'name'})[0].text.replace('"','')
+            brand=i.findAll('span',{'class':'brand'})[0].text.strip()
+            width=i.findAll('div',{'class':'stars'})[0]['style']
+            rating=width.replace('%','').split(': ')[1]
+            revs=i.findAll('div',{'class':'total-ratings'})[0].text
+            for r in [('(',''),(')','')]:
+                revs=revs.replace(*r)
+            a=a.append({
+                'Name':pName,
+                'Brand':brand,
+                'SKU':id,
+                'Link':pLink,
+                'Rating':int(rating),
+                'Reviews':int(revs)}
+                ,ignore_index=True)
+        except (IndexError,KeyError):
+            pass
+    return a
 
 
+df1=pd.DataFrame(columns=('Name','Brand','SKU','Link','Rating','Reviews'))
+for j in range(1,26):
+    url=('https://www.jumia.co.ke/smartphones/?sort=popularity&dir=desc&page={}'.format(j))
+    d1=maxRevFinder(url)
+    df1=pd.concat([df1,d1],ignore_index=True)
 
 
-page=soup(req.get('https://www.jumia.co.ke/smartphones/?sort=popularity').text,'lxml')
-containers=page.findAll('div',{'class':'-gallery'})
-width=containers[0].findAll('div',{'class':'stars'})[0]['style']
-rating=width.replace('%','').split(': ')[1]
+df1.sort_values(by=['Reviews'], inplace=True,ascending=False)
+df1['Rating']=pd.to_numeric(df1['Rating'])
 
-pName=containers[0].findAll('span',{'class':'name'})[0].text.replace('"','')
-type(brand)
+df1['Reviews']=pd.to_numeric(df1['Reviews'])
